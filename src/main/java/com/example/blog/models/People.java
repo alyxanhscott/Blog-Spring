2cd1a416a5fb1535279cac73c0f332ade51c48a9
alyxandriascott <alyxan.hope26@gmail.com>
2018-04-19 09:54:14 -0500

    updated jpa exercise

---
+++ src/main/java/com/example/blog/models/People.java
@@ -0,0 +1,60 @@
+package com.example.blog.models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class People {
+    @Id
+    @GeneratedValue
+    private long id;
+
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    @Column(nullable = false, length = 100)
+    private long age;
+
+    @Column(nullable = false, length = 10)
+    private String state_residing;
+
+    public People(String name, long age, String state_residing) {
+        this.name = name;
+        this.age = age;
+        this.state_residing = state_residing;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getAge() {
+        return age;
+    }
+
+    public void setAge(long age) {
+        this.age = age;
+    }
+
+    public String getState_residing() {
+        return state_residing;
+    }
+
+    public void setState_residing(String state_residing) {
+        this.state_residing = state_residing;
+    }
+}
