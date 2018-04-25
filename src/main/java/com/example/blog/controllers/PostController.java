3c56dcdf5f38f2acbcef2434a645b55d7dc9c436
alyxandriascott <alyxan.hope26@gmail.com>
2018-04-25 15:46:17 -0500

    Removed Merge Junk

--- src/main/java/com/example/blog/controllers/PostController.java
+++ src/main/java/com/example/blog/controllers/PostController.java
@@ -1,4 +1,5 @@
 package com.example.blog.controllers;
+
 import com.example.blog.models.Post;
 import com.example.blog.models.User;
 import com.example.blog.services.PostSvc;
@@ -7,17 +8,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-<<<<<<< HEAD
-import org.springframework.web.bind.annotation.*;
-
-=======
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.validation.Valid;
->>>>>>> security-exercise
 
 @Controller
 public class PostController {
